ead9a375a0bf60ef948cd54e022567b34b8af583
Bony_Chops <office.kentarou@gmail.com>
2020-04-11 02:59:48 +0900

    TEST

--- generate.php
+++ generate.php
@@ -1,6 +1,6 @@
 <?php
 function Generate($str1, $str2){
-    return sprintf('node %s/docs/main.js "%s" "%s"',__DIR__ ,$str1, $str2);
-    exec(sprintf('node %s/docs/main.js "%s" "%s"',__DIR__ ,$str1, $str2));
+    sprintf('node %s/docs/main.js "%s" "%s"',__DIR__ ,$str1, $str2);
+    return exec(sprintf('node %s/docs/main.js "%s" "%s"',__DIR__ ,$str1, $str2));
 }
 ?>
