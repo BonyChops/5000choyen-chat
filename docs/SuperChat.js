b478961b795936b2917fe67755505a464fe5203f
Bony_Chops <office.kentarou@gmail.com>
2020-04-23 19:55:53 +0900

    TEST

--- docs/SuperChat.js
+++ docs/SuperChat.js
@@ -1,13 +1,17 @@
-var express = require('express');
-var router = express.Router();
-var logger = require(__dirname+'/../logger');
+const log4js = require('log4js')
 
-
-router.get('/', function(req, res) {
-  logger.request.info('url:'+ decodeURI(req.url));
-  res.send(200);
+log4js.configure({
+appenders : {
+system : {type : 'file', filename : 'system.log'}
+},
+categories : {
+default : {appenders : ['system'], level : 'debug'},
+}
 });
+const logger = log4js.getLogger('system');
 
+logger.debug('Hello world!');
+//---------------------------
 const Canvas = require('canvas');
 var path = require('path'),
 dataUriToBuffer = require('data-uri-to-buffer'),
