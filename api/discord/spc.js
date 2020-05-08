a9d61da4ffa570afeec030a77d5b22e19fd2d267
Bony_Chops <office.kentarou@gmail.com>
2020-05-08 17:36:25 +0900

    Edit

--- api/discord/spc.js
+++ api/discord/spc.js
@@ -21,6 +21,7 @@ const execWait = (cmd) => {
 }
 
 client.on('message', msg => {
+    console.log(message.author.id);
     if(msg.content.indexOf('!spc') !== -1){
         //console.log('kusa2');
         msg.channel.startTyping();
@@ -44,6 +45,29 @@ client.on('message', msg => {
         msg.delete();
         msg.channel.stopTyping();
     }
+/*     if(msg.author.id == ''){
+        //console.log('kusa2');
+        msg.channel.startTyping();
+        //console.log('kusa');
+        console.log(msg.author.username);
+        console.log(msg.author.avatarURL());
+        if(msg.author.avatarURL() != null){
+            var userIconURL = msg.author.avatarURL();
+        }else{
+            var userIconURL = msg.author.defaultAvatarURL;
+        }
+        let member = msg.guild.member(msg.author);
+        let nickname = member ? member.displayName : msg.author.username;
+        output = execSync('php '+__dirname+'/analyze_spc.php "'+msg.content+'" "'+nickname+'" "'+userIconURL+'"');
+        console.log(output);
+        const jsonObject = JSON.parse(fs.readFileSync(path.resolve(__dirname, './imgur_url.json'), 'utf8'));
+        console.log(jsonObject.url);
+        //sent_mes =  msg.reply('',{files: {jsonObject.url}});
+        const attachment = new MessageAttachment(path.resolve(__dirname, '../../result.png'));
+        msg.reply(attachment);
+        msg.delete();
+        msg.channel.stopTyping();
+    } */
 });
 
 process.on('exit', function (code) {
@@ -59,4 +83,3 @@ process.on('SIGINT', function() {
 const config = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../config.json'), 'utf8'));
 console.log(config.discord.token);
 client.login(config.discord.spc.token); // botログイン
-
